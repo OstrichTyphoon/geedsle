71676768c1c223a0abc7f4706c93b30072e0895f
OstrichTyphoon <52206536+OstrichTyphoon@users.noreply.github.com>
2022-05-10 11:28:44 +0100

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", () => {
+    createSquares();
+
+    let guessedWords = [[]];
+    let availableSpace = 1;
+
+    let word = "dairy"
+
+    const keys = document.querySelectorAll(".keyboard-row button");
+
+    function getCurrentWordArr() {
+        const numberOfGuessedWords = guessedWords.length;
+        return guessedWords[numberOfGuessedWords - 1];
+    }
+
+    function updateGuessedWords(letter) {
+        const currentWordArr = getCurrentWordArr();
+
+        if(currentWordArr && currentWordArr.length < 5)
+            currentWordArr.push(letter)
+
+            const availableSpaceEl = document.getElementById(String(availableSpace))
+            availableSpace = availableSpace + 1
+
+            availableSpaceEl.textContent = letter;
+    }
+
+    function handleSumbitWord() {
+        const currentWordArr =getCurrentWordArr()
+        if (currentWordArr.length !==5) {
+            window.alert("Word must be 5 letters");
+        }
+    }
+
+    function createSquares() {
+        const gameBoard = document.getElementById("board")
+
+        for (let index = 0; index < 30; index++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.setAttribute("id", index + 1);
+            gameBoard.appendChild(square);
+        }
+    }
+
+    for (let i = 0; i < keys.length; i++) {
+        keys[i].onclick = ({ target }) => {
+            const letter = target.getAttribute("data-key");
+
+            if (letter == 'enter') {
+                handleSumbitWord()
+                return;
+            }
+
+            updateGuessedWords(letter);
+        };
+    
+    }
+
+});
